d550513d3f21a2b314f2a00418ad88161763e2de
Ivan Morales <IvanJohnMorales@Gmail.com>
2022-11-14 12:09:51 -0500

    Initial commit

---
+++ src/components/Body.js
@@ -0,0 +1,54 @@
+import React, {useState} from 'react';
+
+const Body = () => {
+    const [base64, setBase64] = useState('');
+
+    const onChange = (e) => {
+        const files = e.target.files;
+        const file = files[0];
+        getBase64(file)
+    }
+
+    const onLoad = (fileString) => {
+        setBase64(fileString)
+    }
+
+    const getBase64 = (file) => {
+        let reader = new FileReader();
+        reader.readAsDataURL(file)
+        reader.onload = () => {
+            onLoad(reader.result)
+        }
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        fetch('ENTER API GATEWAY HERE', {
+            mode: "no-cors",
+            method: "POST",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                senderName: "",
+                senderEmail: "",
+                message: "",
+                base64Data: base64,
+                date: new Date(),
+                fileName: "TEST_FILE_NAME",
+            })
+        })
+    }
+    return (
+        <div>
+            <form>
+                <input type='file' accept='application/pdf' onChange={onChange} />
+            </form>
+            <button onClick={handleSubmit}>SEND TO LAMBDA</button>
+        </div>
+    );
+};
+
+export default Body;
