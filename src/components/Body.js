bd46a22b52e7b79ffb5d2da625b17e68b120c4a1
Ivan Morales <IvanJohnMorales@Gmail.com>
2022-12-02 21:41:43 -0500

    Added AWS services

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,11 +4,18 @@ import "../styles/Body.css";
 
 const Body = () => {
     const [base64, setBase64] = useState('');
+    const [userMessage, setUserMessage] = useState('');
 
     const onChange = (e) => {
         const files = e.target.files;
         const file = files[0];
         getBase64(file)
+    }
+
+    const onTextChange = (e) => {
+        const inputMessage = e.target.value;
+        setUserMessage(inputMessage);
+        console.log(userMessage)
     }
 
     const onLoad = (fileString) => {
@@ -26,7 +33,7 @@ const Body = () => {
     const handleSubmit = (e) => {
         e.preventDefault();
 
-        fetch('ENTER API GATEWAY HERE', {
+        fetch('https://yus2gvnsgk.execute-api.us-east-1.amazonaws.com/sendEmail', {
             mode: "no-cors",
             method: "POST",
             headers: {
@@ -34,9 +41,9 @@ const Body = () => {
                 "Content-Type": "application/json"
             },
             body: JSON.stringify({
-                senderName: "",
-                senderEmail: "",
-                message: "",
+                senderName: "yexovi9626@edinel.com",
+                senderEmail: "yexovi9626@edinel.com",
+                message: userMessage,
                 base64Data: base64,
                 date: new Date(),
                 fileName: "TEST_FILE_NAME",
@@ -50,6 +57,8 @@ const Body = () => {
                 <h3>Send out emails and attachments</h3>
                 <form>
                     <input type='file' accept='application/pdf' onChange={onChange} />
+                    <br></br>
+                    <input type='text' onChange={onTextChange} />
                 </form>
                 <button className='button' onClick={handleSubmit}>SEND TO LAMBDA</button>
             </div>
